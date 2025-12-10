5a131bd054ad2ca755d69a3f84cf6aadee6fb787
Nune Dallakyan <dallakyan_nune@mail.ru>
2025-12-10 16:52:20 +0400

    Floyd–Warshall_mij2.py

---
+++ Floyd–Warshall_mij2.py
@@ -0,0 +1,22 @@
+def floyd_warshall(n, adj):
+    dist = [row[:] for row in adj]
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    return dist
+INF = float('inf')
+
+adj = [
+    [0, 3, INF, 7],
+    [8, 0, 2, INF],
+    [5, INF, 0, 1],
+    [2, INF, INF, 0]
+]
+
+res = floyd_warshall(4, adj)
+for row in res:
+    print(row)
+
